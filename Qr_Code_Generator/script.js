a18f41d07348621748bc0ac64fa8d35b3ef8f3bc
tusharikaJain <143836272+tusharikaJain@users.noreply.github.com>
2025-02-15 19:26:54 +0530

    Add files via upload

---
+++ Qr_Code_Generator/script.js
@@ -0,0 +1,22 @@
+let imgBox=document.getElementById("imgBox");
+let qrImage=document.getElementById("qrImage");
+let qrText=document.getElementById("qrText");
+let btn=document.getElementById("qr");
+
+function generatorQr(){
+  if(qrText.value.length>0){
+    qrImage.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrText.value;
+  imgBox.classList.add("show-img");
+  }
+  else{
+    qrText.classList.add('error');
+    setTimeout(()=>{
+      qrText.classList.remove('error')
+    },1000)
+  }
+  
+}
+
+btn.addEventListener("click",()=>{
+  generatorQr()
+})
